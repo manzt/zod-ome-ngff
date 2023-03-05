168f970a4443c5cd85db9db9e35ee2f1a7a5ef4e
Trevor Manz <trevor.j.manz@gmail.com>
2023-03-05 17:10:47 -0500

    uncomment checks

--- test/latest.test.ts
+++ test/latest.test.ts
@@ -6,11 +6,8 @@ import { gather_test_cases } from "./utils";
 let cases = await gather_test_cases("latest", schemas);
 
 describe.each(cases)("$description - $schema.id", ({ Schema, tests }) => {
-  test.each(tests)("$formerly", ({ formerly, data, valid }) => {
-    // TODO: handle duplicates?
-    let should_skip = formerly.includes("duplicate");
-    let message = valid ? "should be valid" : "should be invalid";
-    it.skipIf(should_skip)(message, () => {
+  test.each(tests)("$formerly", ({ data, valid }) => {
+    it(`should be ${valid ? "valid" : "invalid"}`, () => {
       expect(Schema.safeParse(data).success).toBe(valid);
     });
   });
